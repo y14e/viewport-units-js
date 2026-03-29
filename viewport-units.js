31e3ff91cc965e58401ff7c0d2b3fe57606715d5
Yusuke Kamiyamane <y@y14e.com>
2026-03-29 14:21:54 +0900

    ESLint ➔ Biome

--- viewport-units.js
+++ viewport-units.js
@@ -30,6 +30,6 @@ export function updateViewportUnits(root = document.documentElement) {
     }
     controller.abort();
     observer.disconnect();
-    ['--vw', '--vh', '--vi', '--vb', '--vmin', '--vmax'].forEach((name) => root.style.removeProperty(name));
+    ['--vw', '--vh', '--vi', '--vb', '--vmin', '--vmax'].forEach((name) => void root.style.removeProperty(name));
   };
 }
